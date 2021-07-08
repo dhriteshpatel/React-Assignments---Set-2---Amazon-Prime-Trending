5016e980499143a0113ae255a674a58f50a5f316
dhriteshpatel <33549752+dhriteshpatel@users.noreply.github.com>
2021-07-08 17:25:44 +0530

    last update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
+  const [counter, setCounter] = useState(0);
+  const [data] = useState(props.slides);
+
+ 
   return (
-    <>
-    <h1>Hii</h1>
-    </>
+    <div>
+    <h1 data-testid="title">{data[counter].title}</h1>
+    <p data-testid="text">{data[counter].text}</p>
+    <button data-testid="button-next" disabled={counter === props.slides.length-1 ? true : false} onClick={() =>setCounter(counter+1)}>Next</button>
+    <button data-testid="button-prev" disabled={counter === 0 ? true : false} onClick={() => setCounter(counter-1)}>Prev</button>
+    <button data-testid="button-restart" onClick={() =>setCounter(0)}>Restart</button>
+    </div>
   )
 }
 
